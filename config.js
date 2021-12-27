973e6c36649a54d0f907306691b233a4dec78500
aviaspot <info@aviaspot.com>
2021-12-27 17:26:04 +0600

    Web App Deploy

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDh6lY7b4nGJsFuwMumISKFJftoK3zWVsk",
+    authDomain: "aviaspot-c9862.firebaseapp.com",
+    projectId: "aviaspot-c9862",
+    storageBucket: "aviaspot-c9862.appspot.com",
+    messagingSenderId: "891643271094",
+    appId: "1:891643271094:web:7fb0e4950c7a97ce2f1317"
+  };
---
+++ remove_language.js
@@ -0,0 +1,5 @@
+if (navigator.language.includes("en")) {
+  document.getElementById("en").style.visibility = "visible";
+} else {
+  document.getElementById("ru").style.visibility = "visible";
+}
